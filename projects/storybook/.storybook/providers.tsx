25ce22481c7ece2133b77f317f8a063fc7b6c936
Lee-Seungje <moondgod@gmail.com>
2023-08-23 22:33:31 +0900

    Delete annotate

--- projects/storybook/.storybook/providers.tsx
+++ projects/storybook/.storybook/providers.tsx
@@ -1,11 +1,10 @@
-"use client";
+'use client';
 
-import React from "react";
+import React from 'react';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  // TODO: client api 연동 시, queryclient option 공유 가능하도록 구성 변경
   const [queryClient] = React.useState(
     () =>
       new QueryClient({
