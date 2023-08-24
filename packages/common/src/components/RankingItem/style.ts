d694e8a40689ed196c7f168510b5c12f29fe4aa3
Lee-Seungje <moondgod@gmail.com>
2023-08-24 23:17:50 +0900

    Add style

---
+++ packages/common/src/components/RankingItem/style.ts
@@ -0,0 +1,54 @@
+import styled from '@emotion/styled';
+
+import Image from 'next/image';
+
+export const ItemWrapper = styled.div`
+  width: 540px;
+  height: 70px;
+  border-radius: 10px;
+  display: flex;
+  padding: 0 44px;
+  align-items: center;
+  gap: 231px;
+  transition: ease-in-out 0.2s;
+  cursor: pointer;
+
+  &:hover {
+    background: #ffd79b;
+  }
+`;
+
+export const FlexWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Ranking = styled.span`
+  ${({ theme }) => theme.typo.h3};
+  color: ${({ theme }) => theme.color.black};
+  font-weight: 500;
+`;
+
+export const ProfileImage = styled(Image)`
+  border-radius: 50%;
+  margin: 0 16px 0 20px;
+`;
+
+export const UserName = styled.span`
+  ${({ theme }) => theme.typo.h4};
+  color: ${({ theme }) => theme.color.black};
+  font-weight: 500;
+`;
+
+export const Point = styled.span`
+  ${({ theme }) => theme.typo.button};
+  color: ${({ theme }) => theme.color.black};
+  font-weight: 500;
+  margin-right: 4px;
+`;
+
+export const PointUnit = styled.span`
+  ${({ theme }) => theme.typo.body2};
+  color: ${({ theme }) => theme.color.black};
+  font-weight: 500;
+`;
