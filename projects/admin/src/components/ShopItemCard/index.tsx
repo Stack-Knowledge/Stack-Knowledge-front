c67a59642b7bb3a53361b31de467799ff69c8c8b
Lee-Seungje <moondgod@gmail.com>
2023-08-27 16:12:58 +0900

    Add shopItemCard

---
+++ projects/admin/src/components/ShopItemCard/index.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import * as S from './style';
+
+import Image from 'next/image';
+
+import { slicePoint } from 'common';
+
+import { OrderdItemType } from 'types';
+
+const ShopItemCard: React.FC<OrderdItemType> = ({
+  item,
+  price,
+  count,
+  user,
+}) => (
+  <S.CardWrapper>
+    <S.ImageWrapper>
+      <Image src={item.image} alt='item image' fill />
+    </S.ImageWrapper>
+    <S.TextWrapper>
+      <S.UserName>{user.name}</S.UserName>
+      <S.Details>{count}</S.Details>
+    </S.TextWrapper>
+    <S.TextWrapper>
+      <S.Details>{item.name}</S.Details>
+      <S.PriceWrapper>
+        <S.Price>{slicePoint(price)}</S.Price>
+        <S.WonText>원</S.WonText>
+      </S.PriceWrapper>
+    </S.TextWrapper>
+  </S.CardWrapper>
+);
+
+export default ShopItemCard;
