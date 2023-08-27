00c4493e58582923631def50301c9ec339e6bf95
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-08-27 17:34:38 +0900

    add svg MadeIcon

---
+++ packages/common/src/assets/svg/MadeIcon.tsx
@@ -0,0 +1,30 @@
+const MadeIcon = () => (
+  <svg
+    width='28'
+    height='28'
+    viewBox='0 0 28 28'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <circle cx='14' cy='14' r='13.4' stroke='black' stroke-width='1.2' />
+    <rect
+      x='13.4399'
+      y='7.84009'
+      width='1.12'
+      height='12.32'
+      rx='0.56'
+      fill='black'
+    />
+    <rect
+      x='7.83984'
+      y='14.5601'
+      width='1.12'
+      height='12.32'
+      rx='0.56'
+      transform='rotate(-90 7.83984 14.5601)'
+      fill='black'
+    />
+  </svg>
+);
+
+export default MadeIcon;
