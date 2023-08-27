1a89d6a4e3a2aa71075ba70d386608ecdf8db68c
Lee-Seungje <moondgod@gmail.com>
2023-08-27 17:37:04 +0900

    Update px -> rem

--- projects/admin/src/components/Timer/style.ts
+++ projects/admin/src/components/Timer/style.ts
@@ -7,19 +7,19 @@ export const Wrapper = styled.div`
 `;
 
 export const TimerWrapper = styled.div`
-  width: 200px;
-  height: 200px;
+  width: 12.5rem;
+  height: 12.5rem;
   border-radius: 50%;
-  box-shadow: 0 0 0 4px ${({ theme }) => theme.color.primary} inset;
+  box-shadow: 0 0 0 0.25rem ${({ theme }) => theme.color.primary} inset;
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 4.7px;
+  gap: 0.2938rem;
 `;
 
 export const Collon = styled.div`
   ${({ theme }) => theme.typo.h1};
-  height: 56px;
+  height: 3.5rem;
   color: ${({ theme }) => theme.color.black};
   font-weight: 500;
   display: flex;
@@ -28,8 +28,8 @@ export const Collon = styled.div`
 
 export const Input = styled.input`
   ${({ theme }) => theme.typo.h1};
-  width: 50px;
-  height: 56px;
+  width: 3.125rem;
+  height: 3.5rem;
   color: ${({ theme }) => theme.color.black};
   font-weight: 500;
   border: 0;
@@ -43,5 +43,5 @@ export const NoticeText = styled.p`
   ${({ theme }) => theme.typo.body2};
   color: ${({ theme }) => theme.color.gray['070']};
   font-weight: 400;
-  margin: 20px 0 0;
+  margin: 1.25rem 0 0;
 `;
