8552440e2c8b93983489c7b0353e46c9c447180d
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-08-31 15:34:08 +0900

    Fix svg path

--- packages/common/src/assets/svg/QuestionIcon.tsx
+++ packages/common/src/assets/svg/QuestionIcon.tsx
@@ -9,26 +9,26 @@ const QuestionIcon = () => (
     <path
       d='M2.125 21H17.875C18.4963 21 19 20.5523 19 20V6L13.9375 1H2.125C1.50368 1 1 1.44771 1 2V20C1 20.5523 1.50368 21 2.125 21Z'
       stroke='black'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
     <path
       d='M10 14.5V13C11.2426 13 12.25 11.9927 12.25 10.75C12.25 9.50735 11.2426 8.5 10 8.5C8.75738 8.5 7.75 9.50735 7.75 10.75'
       stroke='black'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
     <path
-      fill-rule='evenodd'
-      clip-rule='evenodd'
+      fillRule='evenodd'
+      clipRule='evenodd'
       d='M10 17.5C10.466 17.5 10.8438 17.1642 10.8438 16.75C10.8438 16.3358 10.466 16 10 16C9.53401 16 9.15625 16.3358 9.15625 16.75C9.15625 17.1642 9.53401 17.5 10 17.5Z'
       fill='black'
     />
     <path
       d='M13.375 1V6H19'
       stroke='black'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
   </svg>
 );
