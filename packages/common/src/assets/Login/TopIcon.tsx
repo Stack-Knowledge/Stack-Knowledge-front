a17391cb608f240a82025f13e9bc50d5df40797a
Lee-Seungje <moondgod@gmail.com>
2023-08-31 13:45:58 +0900

    Add images

---
+++ packages/common/src/assets/Login/TopIcon.tsx
@@ -0,0 +1,18 @@
+const TopIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='526'
+    height='361'
+    viewBox='0 0 526 361'
+    fill='none'
+  >
+    <path
+      fill-rule='evenodd'
+      clip-rule='evenodd'
+      d='M0 320.445C43.9702 345.32 90.1646 360.162 135.5 360.162C256.517 360.162 423.042 175.627 525.448 0H0V320.445Z'
+      fill='#FFC48E'
+    />
+  </svg>
+);
+
+export default TopIcon;
