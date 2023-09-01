d5f8a420c0c7fd0b7e63b5fc49bfd68160ff2ca3
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-02 00:25:07 +0900

    Update setting svg

--- projects/storybook/.storybook/main.js
+++ projects/storybook/.storybook/main.js
@@ -1,32 +1,32 @@
-import { join, dirname } from "path";
+import { join, dirname } from 'path';
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
 
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
   stories: [
-    "../../client/src/components/**!(node_modules)/*.stories.@(js|jsx|ts|tsx)",
-    "../../admin/src/components/**!(node_modules)/*.stories.@(js|jsx|ts|tsx)",
-    "../../../packages/common/src/components/**!(node_modules)/*.stories.@(js|jsx|ts|tsx)",
+    '../../client/src/components/**!(node_modules)/*.stories.@(js|jsx|ts|tsx)',
+    '../../admin/src/components/**!(node_modules)/*.stories.@(js|jsx|ts|tsx)',
+    '../../../packages/common/src/components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-interactions'),
   ],
   framework: {
-    name: getAbsolutePath("@storybook/nextjs"),
+    name: getAbsolutePath('@storybook/nextjs'),
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
 };
 export default config;
