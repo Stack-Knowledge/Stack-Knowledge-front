c424bc8e4e85b58f1272fe98a87d414b3a1f1821
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-02 00:28:12 +0900

    Add banner1

--- packages/common/src/components/Banner/Banner1/index.tsx
+++ packages/common/src/components/Banner/Banner1/index.tsx
@@ -1,8 +1,19 @@
-`use client`;
+'use client';
 
 import * as S from './style';
 import { SunIcon, LetGoIcon } from '../../../assets';
 
-const Banner1 = () => <S.BannerWrapper>d</S.BannerWrapper>;
-
+const Banner1 = () => (
+  <S.BannerWrapper>
+    <S.BannerTitleContainer>
+      <S.BannerTitle>문제만 풀어도 외출이 가능하다고??</S.BannerTitle>
+      <S.SunIconWrapper>
+        <SunIcon />
+      </S.SunIconWrapper>
+    </S.BannerTitleContainer>
+    <S.LetgoIconWrapper>
+      <LetGoIcon />
+    </S.LetgoIconWrapper>
+  </S.BannerWrapper>
+);
 export default Banner1;
--- packages/common/src/components/Banner/Banner1/style.ts
+++ packages/common/src/components/Banner/Banner1/style.ts
@@ -1,6 +1,33 @@
 import styled from '@emotion/styled';
 
 export const BannerWrapper = styled.div`
+  background: ${({ theme }) => theme.color.banner1};
   width: 80rem;
   height: 18.75rem;
+  border-radius: 1.25rem;
+  display: flex;
+`;
+
+export const BannerTitleContainer = styled.div`
+  width: 40rem;
+  height: 18.75rem;
+  padding: 4.375rem 0 0 3.75rem;
+`;
+
+export const BannerTitle = styled.span`
+  color: ${({ theme }) => theme.color.white};
+  ${({ theme }) => theme.typo.h1};
+  font-weight: 700;
+`;
+
+export const SunIconWrapper = styled.div`
+  position: relative;
+  bottom: 0.625rem;
+  right: 1.25rem;
+`;
+
+export const LetgoIconWrapper = styled.div`
+  width: 40rem;
+  height: 18.75rem;
+  padding: 1.5rem 3.75rem 1rem 11.25rem;
 `;
