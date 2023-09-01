2e6367dc47e12b202dc9a551930de7f9ddc13cdb
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-01 18:33:06 +0900

    Update banner svg settings

--- packages/common/src/assets/index.ts
+++ packages/common/src/assets/index.ts
@@ -1,1 +1,5 @@
 export * from './svg';
+export { default as DocumentIcon } from './Banner/DocumentIcon';
+export { default as GiftIcon } from './Banner/GiftIcon';
+export { default as LetGoIcon } from './Banner/LetGoIcon';
+export { default as SunIcon } from './Banner/SunIcon';
