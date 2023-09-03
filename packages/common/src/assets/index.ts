cb5fa4c981539a7b843fcf60c70a5993fbebc6a8
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-03 22:41:23 +0900

    Deleted svg index.ts

--- packages/common/src/assets/index.ts
+++ packages/common/src/assets/index.ts
@@ -1,5 +1,1 @@
 export * from './svg';
-export { default as DocumentIcon } from './Banner/DocumentIcon';
-export { default as GiftIcon } from './Banner/GiftIcon';
-export { default as LetGoIcon } from './Banner/LetGoIcon';
-export { default as SunIcon } from './Banner/SunIcon';
