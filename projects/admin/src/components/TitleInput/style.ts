b74b85d458294bebe95f8ac4287f90eb46a4217a
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-06 20:45:48 +0900

    Update unit change px -> rem

--- projects/admin/src/components/TitleInput/style.ts
+++ projects/admin/src/components/TitleInput/style.ts
@@ -1,14 +1,14 @@
 import styled from '@emotion/styled';
 
 export const TitleInput = styled.input`
-  width: 950px;
-  height: 100px;
-  border-radius: 10px;
+  width: 59.375rem;
+  height: 6.25rem;
+  border-radius: 0.625rem;
 
   outline: none;
-  text-indent: 18px;
+  text-indent: 1.125rem;
   border: 0;
 
   background: ${({ theme }) => theme.color.gray['010']};
-  box-shadow: 3px 3px 4px 0px rgba(120, 120, 120, 0.25);
+  box-shadow: 0.1875rem 0.1875rem 0.25rem 0rem rgba(120, 120, 120, 0.25);
 `;
