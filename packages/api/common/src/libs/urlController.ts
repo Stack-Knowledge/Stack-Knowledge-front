45edd9369bf6e5719e125cc7540cf4c8d1fcc928
Lee-Seungje <moondgod@gmail.com>
2023-09-06 11:44:39 +0900

    Add refresh

--- packages/api/common/src/libs/urlController.ts
+++ packages/api/common/src/libs/urlController.ts
@@ -2,3 +2,7 @@ export const missionUrl = {
   missionList: () => `/mission`,
   missionDetail: (missionId: string) => `/mission/${missionId}`,
 } as const;
+
+export const authUrl = {
+  refresh: () => `/auth`,
+} as const;
