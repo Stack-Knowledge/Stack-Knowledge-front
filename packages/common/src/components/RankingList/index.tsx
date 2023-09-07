ed638fb4fd3f0606d4e0c590bcde0cdea84bbcb2
Lee-Seungje <moondgod@gmail.com>
2023-09-07 19:51:46 +0900

    Add rankingList

---
+++ packages/common/src/components/RankingList/index.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import * as S from './style';
+
+import { RankingItem } from 'common/components';
+
+import { RankingPropsType } from 'types';
+
+interface RankingListProps {
+  list: RankingPropsType[];
+}
+
+const RankingList: React.FC<RankingListProps> = ({ list }) => (
+  <S.RankingListWrapper>
+    {list.map((item, index) => (
+      <>
+        <RankingItem key={item.id} item={item} ranking={index + 1} />
+        <S.Line />
+      </>
+    ))}
+  </S.RankingListWrapper>
+);
+
+export default RankingList;
