739aed24e62c834bf923e00d21460ac08353b62d
Lee-Seungje <moondgod@gmail.com>
2023-09-07 15:42:03 +0900

    Delete useless code

--- projects/admin/src/app/page.tsx
+++ projects/admin/src/app/page.tsx
@@ -1,12 +1,3 @@
-"use client";
-
-import { Example } from "common";
-
 export default function Home() {
-  return (
-    <>
-      <Example />
-      <div>admin</div>
-    </>
-  );
+  return <></>;
 }
