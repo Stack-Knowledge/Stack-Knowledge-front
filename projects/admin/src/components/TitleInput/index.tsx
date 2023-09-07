56b2b8d59351214740a4620d9e3f321fb8445cbf
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-08 01:04:13 +0900

    Update font-size appoint

--- projects/admin/src/components/TitleInput/index.tsx
+++ projects/admin/src/components/TitleInput/index.tsx
@@ -2,6 +2,8 @@
 
 import * as S from './style';
 
-const TitleInput = () => <S.TitleInput />;
+const TitleInput = () => (
+  <S.TitleInput placeholder='제목을 작성해 주시기 바랍니다.' />
+);
 
 export default TitleInput;
--- projects/admin/src/components/TitleInput/style.ts
+++ projects/admin/src/components/TitleInput/style.ts
@@ -1,14 +1,13 @@
 import styled from '@emotion/styled';
 
 export const TitleInput = styled.input`
+  ${({ theme }) => theme.typo.body1};
+  background: ${({ theme }) => theme.color.gray['010']};
   width: 59.375rem;
   height: 6.25rem;
   border-radius: 0.625rem;
-
   outline: none;
   text-indent: 1.125rem;
   border: 0;
-
-  background: ${({ theme }) => theme.color.gray['010']};
   box-shadow: 0.1875rem 0.1875rem 0.25rem 0rem rgba(120, 120, 120, 0.25);
 `;
