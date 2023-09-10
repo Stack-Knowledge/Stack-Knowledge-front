132948ea800884479fa9603a7928fcb5f62f761f
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-11 02:13:34 +0900

    Add export common submitInput

--- packages/common/src/components/index.ts
+++ packages/common/src/components/index.ts
@@ -7,3 +7,4 @@ export { default as Banner1 } from './Banner/Banner1';
 export { default as Banner2 } from './Banner/Banner2';
 export { default as Banner3 } from './Banner/Banner3';
 export { default as Header } from './Header';
+export { default as SubmitInput } from './SubmitInput';
