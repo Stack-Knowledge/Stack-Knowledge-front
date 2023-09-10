432a98c4775b12d80df352eba5b85c2d19595b0a
Lee-Seungje <moondgod@gmail.com>
2023-09-10 12:23:22 +0900

    Add vector icon

---
+++ packages/common/src/assets/svg/VectorIcon.tsx
@@ -0,0 +1,27 @@
+const VectorIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='80'
+    height='80'
+    viewBox='0 0 80 80'
+    fill='none'
+  >
+    <circle cx='40' cy='40' r='40' fill='#D9D9D9' />
+    <rect
+      width='2.20452'
+      height='24.2497'
+      rx='1.10226'
+      transform='matrix(0.74877 0.66283 -0.74877 0.66283 44.3496 24)'
+      fill='white'
+    />
+    <rect
+      width='2.20452'
+      height='23.7263'
+      rx='1.10226'
+      transform='matrix(0.74877 -0.66283 0.74877 0.66283 26 40.2734)'
+      fill='white'
+    />
+  </svg>
+);
+
+export default VectorIcon;
