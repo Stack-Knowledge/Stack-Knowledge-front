1c5cabc092376ceaba9b115c1f1f6a0fde3a8898
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-10 16:55:16 +0900

    Update inputLength, style maxlength fontColor

--- projects/admin/src/components/TitleInput/index.tsx
+++ projects/admin/src/components/TitleInput/index.tsx
@@ -1,9 +1,26 @@
 `use client`;
 
+import { useState } from 'react';
 import * as S from './style';
 
-const TitleInput = () => (
-  <S.TitleInput placeholder='제목을 작성해 주시기 바랍니다.' maxLength={20} />
-);
+const TitleInput = () => {
+  const [inputCount, setInputCount] = useState<number>(0);
+
+  const onInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    setInputCount(inputValue.length <= 20 ? inputValue.length : 20);
+  };
+
+  return (
+    <S.TitleInputWrapper inputContent={inputCount}>
+      <S.TitleInput
+        placeholder='제목을 작성해 주시기 바랍니다.'
+        maxLength={20}
+        onChange={onInputHandler}
+      />
+      <span>{inputCount} / 20</span>
+    </S.TitleInputWrapper>
+  );
+};
 
 export default TitleInput;
--- projects/admin/src/components/TitleInput/style.ts
+++ projects/admin/src/components/TitleInput/style.ts
@@ -1,13 +1,30 @@
 import styled from '@emotion/styled';
 
+export const TitleInputWrapper = styled.div<{ inputContent: number }>`
+  background: ${({ theme }) => theme.color.gray['010']};
+  width: 59.375rem;
+  height: 6.25rem;
+  display: flex;
+  border-radius: 0.625rem;
+  align-items: center;
+  box-shadow: 0.1875rem 0.1875rem 0.25rem 0rem rgba(120, 120, 120, 0.25);
+
+  span {
+    color: ${(props) =>
+      props.inputContent === 20
+        ? 'var(--maincolor, #FFA927)'
+        : 'var(--darkgray_3, #6c6c6c)'};
+  }
+`;
+
 export const TitleInput = styled.input`
   ${({ theme }) => theme.typo.body1};
   background: ${({ theme }) => theme.color.gray['010']};
-  width: 59.375rem;
+  color: ${({ theme }) => theme.color.black};
+  width: 54.25rem;
   height: 6.25rem;
   border-radius: 0.625rem;
   outline: none;
   text-indent: 1.125rem;
   border: 0;
-  box-shadow: 0.1875rem 0.1875rem 0.25rem 0rem rgba(120, 120, 120, 0.25);
 `;
