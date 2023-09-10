5fb7a4632caba6d0f164c8a64fcb358ad0f8b62b
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-11 03:14:30 +0900

    Update deleted fragment

--- projects/admin/src/PageContainer/CreatePage/index.tsx
+++ projects/admin/src/PageContainer/CreatePage/index.tsx
@@ -5,21 +5,19 @@ import { SubmitInput } from 'common';
 
 import * as S from './style';
 const CreatePage = () => (
-  <>
-    <S.PageWrapper>
-      <div>
-        <S.TimerWrapper>
-          <Timer />
-        </S.TimerWrapper>
-        <S.Section>[제목]</S.Section>
-        <TitleInput />
-        <S.MissionWrapper>
-          <S.Section>[문제]</S.Section>
-          <SubmitInput role='admin' />
-        </S.MissionWrapper>
-      </div>
-    </S.PageWrapper>
-  </>
+  <S.PageWrapper>
+    <div>
+      <S.TimerWrapper>
+        <Timer />
+      </S.TimerWrapper>
+      <S.Section>[제목]</S.Section>
+      <TitleInput />
+      <S.MissionWrapper>
+        <S.Section>[문제]</S.Section>
+        <SubmitInput role='admin' />
+      </S.MissionWrapper>
+    </div>
+  </S.PageWrapper>
 );
 
 export default CreatePage;
