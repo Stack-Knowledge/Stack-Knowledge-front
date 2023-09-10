e98995be2cf9c767cfa7ec4bc98dc0747467fb67
Lee-Seungje <moondgod@gmail.com>
2023-09-10 14:31:07 +0900

    Add mission carousel

---
+++ packages/common/src/components/MissionCarousel/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const CarouselWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 48px;
+`;
+
+export const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 2rem 1.5rem;
+`;
