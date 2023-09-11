cea923e848a18712626ac8089328c902d13a6b52
Lee-Seungje <moondgod@gmail.com>
2023-09-12 00:17:44 +0900

    Add shop page

--- projects/client/src/PageContainer/ShopPage/index.tsx
+++ projects/client/src/PageContainer/ShopPage/index.tsx
@@ -2,6 +2,17 @@
 
 import * as S from './style';
 
-const ShopPage = () => <div></div>;
+import { slicePoint } from 'common';
+
+const ShopPage = () => (
+  <S.PageWrapper>
+    <S.MiledgeTitle>현재 마일리지</S.MiledgeTitle>
+    <S.FlexWrapper>
+      <S.MilidgePoint>{slicePoint(1000)}</S.MilidgePoint>
+      <S.MilidgeUnit>M</S.MilidgeUnit>
+    </S.FlexWrapper>
+    <S.SelectButton>선택하기</S.SelectButton>
+  </S.PageWrapper>
+);
 
 export default ShopPage;
--- projects/client/src/PageContainer/ShopPage/style.ts
+++ projects/client/src/PageContainer/ShopPage/style.ts
@@ -1,1 +1,45 @@
 import styled from '@emotion/styled';
+
+export const PageWrapper = styled.div`
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 60px;
+`;
+
+export const MiledgeTitle = styled.h2`
+  ${({ theme }) => theme.typo.h2};
+  color: ${({ theme }) => theme.color.black};
+  font-weight: 500;
+`;
+
+export const FlexWrapper = styled.div`
+  display: flex;
+  gap: 4px;
+  margin-top: 27px;
+`;
+
+export const MilidgePoint = styled.span`
+  ${({ theme }) => theme.typo.title};
+  color: ${({ theme }) => theme.color.black};
+  font-weight: 500;
+`;
+
+export const MilidgeUnit = styled(MiledgeTitle)`
+  margin-top: 15px;
+`;
+
+export const SelectButton = styled.button`
+  ${({ theme }) => theme.typo.button};
+  color: ${({ theme }) => theme.color.white};
+  font-weight: 600;
+
+  width: 400px;
+  height: 70px;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.color.primary};
+  border: 0;
+  cursor: pointer;
+  margin-top: 100px;
+`;
