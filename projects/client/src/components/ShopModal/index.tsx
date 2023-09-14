963194f6dfcc43f497c16a7495f17f377bec85ba
Lee-Seungje <moondgod@gmail.com>
2023-09-15 00:38:30 +0900

    Add form

--- projects/client/src/components/ShopModal/index.tsx
+++ projects/client/src/components/ShopModal/index.tsx
@@ -7,7 +7,9 @@ import { ShopItemType } from 'types';
 const ShopItem = () => (
   <S.ModalWrapper>
     <S.ModalTitle>선택하신 상품이 맞으십니까?</S.ModalTitle>
-    <S.PurchusButton>구매하기</S.PurchusButton>
+    <form method='dialog'>
+      <S.PurchusButton>구매하기</S.PurchusButton>
+    </form>
   </S.ModalWrapper>
 );
 
