89711e0d2ceda7f60c687a1000a69915357271bb
Lee-Seungje <moondgod@gmail.com>
2023-09-15 00:14:38 +0900

    Add icons

--- projects/client/src/assets/index.ts
+++ projects/client/src/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as CheckedBoxIcon } from './svg/CheckedBoxIcon';
 export { default as CheckBoxIcon } from './svg/CheckBoxIcon';
+export { default as MinusIcon } from './svg/MinusIcon';
+export { default as PlusIcon } from './svg/MinusIcon';
---
+++ projects/client/src/assets/svg/MinusIcon.tsx
@@ -0,0 +1,20 @@
+const MinusIcon = () => (
+  <svg
+    width='14'
+    height='2'
+    viewBox='0 0 14 2'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <rect
+      y='1.63635'
+      width='1.27273'
+      height='14'
+      rx='0.636364'
+      transform='rotate(-90 0 1.63635)'
+      fill='black'
+    />
+  </svg>
+);
+
+export default MinusIcon;
