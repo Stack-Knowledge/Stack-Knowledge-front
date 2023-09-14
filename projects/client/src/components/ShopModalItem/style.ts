b1cc98a01d407909155fe2efe0fb8145c7f5d39d
Lee-Seungje <moondgod@gmail.com>
2023-09-15 00:08:38 +0900

    Add style

---
+++ projects/client/src/components/ShopModalItem/style.ts
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+
+export const ItemWrapper = styled.div`
+  width: 400px;
+  height: 80px;
+  border-radius: 10px;
+  background: #ffd79b;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 0 18px;
+  justify-content: space-between;
+`;
+
+export const FlexBox = styled.div`
+  display: flex;
+  gap: 4px;
+`;
+
+export const TextBox = styled(FlexBox)`
+  flex-direction: column;
+  gap: 6px;
+`;
+
+export const ItemName = styled.span`
+  ${({ theme }) => theme.typo.button};
+  color: ${({ theme }) => theme.color.black};
+  font-weight: 500;
+`;
+
+export const PointText = styled(ItemName)`
+  ${({ theme }) => theme.typo.body1};
+`;
+
+export const MilidgeUnit = styled(ItemName)`
+  ${({ theme }) => theme.typo.body2};
+  font-weight: 400;
+  margin-top: 1px;
+`;
