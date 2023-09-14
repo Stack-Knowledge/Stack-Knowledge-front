89c2914bc8c5bb4c269b53f230c3982c09ac932d
Lee-Seungje <moondgod@gmail.com>
2023-09-15 08:20:46 +0900

    Update modal story

--- projects/client/src/components/ShopModal/index.stories.ts
+++ projects/client/src/components/ShopModal/index.stories.ts
@@ -16,4 +16,25 @@ export default {
 
 type Story = StoryObj<typeof ShopModal>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    selectedList: [
+      {
+        itemId: 'id',
+        name: '자장면1',
+        text: '자장면',
+        price: 5000,
+        image:
+          'https://mblogthumb-phinf.pstatic.net/MjAxNzAxMjNfNDkg/MDAxNDg1MTYwMTQ1MDc5.H7I84KnCb0_U0Fb312NMMk10dbmNhdMb45jDiNYs13Eg.e3185KBkHafPDJpOjsZ7vvvH741l0TzEt2vVNvePS7Mg.JPEG.china_lab/shutterstock_524181190.jpg?type=w800',
+      },
+      {
+        itemId: 'id',
+        name: '자장면2',
+        text: '자장면',
+        price: 5000,
+        image:
+          'https://mblogthumb-phinf.pstatic.net/MjAxNzAxMjNfNDkg/MDAxNDg1MTYwMTQ1MDc5.H7I84KnCb0_U0Fb312NMMk10dbmNhdMb45jDiNYs13Eg.e3185KBkHafPDJpOjsZ7vvvH741l0TzEt2vVNvePS7Mg.JPEG.china_lab/shutterstock_524181190.jpg?type=w800',
+      },
+    ],
+  },
+};
