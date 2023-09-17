20d42babb4337d90593aa1a6556469ca0a6fd3b1
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-17 23:40:58 +0900

    Update import store workspace

--- projects/admin/src/PageContainer/CreatePage/index.tsx
+++ projects/admin/src/PageContainer/CreatePage/index.tsx
@@ -3,22 +3,25 @@
 import { MissionDetailInput } from 'common';
 
 import { Timer, TitleInput } from 'admin/components';
+import { useRef } from 'react';
 import * as S from './style';
 
-const CreatePage = () => (
-  <S.PageWrapper>
-    <div>
-      <S.TimerWrapper>
-        <Timer />
-      </S.TimerWrapper>
-      <S.Section>[제목]</S.Section>
-      <TitleInput />
-      <S.MissionWrapper>
-        <S.Section>[문제]</S.Section>
-        <MissionDetailInput role='admin' />
-      </S.MissionWrapper>
-    </div>
-  </S.PageWrapper>
-);
+const CreatePage = () => {
+  return (
+    <S.PageWrapper>
+      <div>
+        <S.TimerWrapper>
+          <Timer />
+        </S.TimerWrapper>
+        <S.Section>[제목]</S.Section>
+        <TitleInput />
+        <S.MissionWrapper>
+          <S.Section>[문제]</S.Section>
+          <MissionDetailInput role='admin' />
+        </S.MissionWrapper>
+      </div>
+    </S.PageWrapper>
+  );
+};
 
 export default CreatePage;
