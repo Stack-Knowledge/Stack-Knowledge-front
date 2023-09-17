c601eb4cd00d0b4900b08e71aca4d3200548a883
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-17 22:20:15 +0900

    Update px -> rem

--- projects/client/src/components/MissionDetailModal/style.ts
+++ projects/client/src/components/MissionDetailModal/style.ts
@@ -3,53 +3,50 @@ import styled from '@emotion/styled';
 export const ModalWrapper = styled.div`
   background-color: ${({ theme }) => theme.color.white};
   ${({ theme }) => theme.color.black};
-
-  width: 280px;
-  height: 150px;
-  box-shadow: 4px 4px 10px 0px rgba(169, 169, 169, 0.6);
-  border-radius: 20px;
+  width: 17.5rem;
+  height: 9.375rem;
+  box-shadow: 0.25rem 0.25rem 0.625rem 0rem rgba(169, 169, 169, 0.6);
+  border-radius: 1.25rem;
 `;
 
 export const ModalContentWrapper = styled.div`
   display: flex;
-  height: 98px;
-
+  height: 6.125rem;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 `;
 
 export const ModalContent = styled.span`
-  font-weight: 500;
   color: ${({ theme }) => theme.color.black};
+  font-weight: 500;
 `;
 
 export const ModalButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
-  gap: 16px;
+  gap: 1rem;
 `;
 
 export const ModalDeleted = styled.button`
   background-color: ${({ theme }) => theme.color.primary};
   color: ${({ theme }) => theme.color.white};
   ${({ theme }) => theme.typo.body2};
-  width: 116px;
-  border-radius: 10px;
+  width: 7.25rem;
+  border-radius: 0.625rem;
   border: 0;
-  padding: 10px;
-  margin-left: 8px;
+  padding: 0.625rem;
+  margin-left: 0.5rem;
   cursor: pointer;
 `;
 export const ModalConfirm = styled.button`
   background-color: ${({ theme }) => theme.color.white};
-  border: 1px solid ${({ theme }) => theme.color.primary};
+  border: 0.0625rem solid ${({ theme }) => theme.color.primary};
   color: ${({ theme }) => theme.color.primary};
   ${({ theme }) => theme.typo.body2};
-  width: 116px;
-  border-radius: 10px;
-  padding: 10px;
-
-  margin-right: 8px;
+  width: 7.25rem;
+  border-radius: 0.625rem;
+  padding: 0.625rem;
+  margin-right: 0.5rem;
   cursor: pointer;
 `;
