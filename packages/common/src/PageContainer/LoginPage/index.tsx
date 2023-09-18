ab21021eb501898b9954470c65d1bd0bc579b9c1
Lee-Seungje <moondgod@gmail.com>
2023-09-18 15:03:55 +0900

    Add GAuth URL

--- packages/common/src/PageContainer/LoginPage/index.tsx
+++ packages/common/src/PageContainer/LoginPage/index.tsx
@@ -5,6 +5,11 @@ import { LoginButton } from 'common/components';
 import { BottomIcon, TopIcon, LoginLogoIcon } from 'common/assets';
 
 const LoginPage = () => {
+  const handleLogin = () => {
+    window.location.href = `https://gauth.co.kr/login?client_id=e6e8ac7857c94ca7a24db504d33369078ab562d7a29a4c9db353204ae8080be9&redirect_uri=http://localhost:8080/login/oauth/code/gauth'`;
+  };
+
+  // `https://gauth.co.kr/login?client_id=${'e6e8ac7857c94ca7a24db504d33369078ab562d7a29a4c9db353204ae8080be9'}&redirect_uri=${'https://port-0-stack-knowledge-server-1xxfe2bllyrfbtt.sel5.cloudtype.app/'}`
   return (
     <S.MainWrapper>
       <S.TopImageWrapper>
@@ -15,7 +20,7 @@ const LoginPage = () => {
       </S.BottomIconWrapper>
       <LoginLogoIcon />
       <S.StackKnowledge>Stack Knowledge</S.StackKnowledge>
-      <LoginButton />
+      <LoginButton onClick={handleLogin} />
     </S.MainWrapper>
   );
 };
