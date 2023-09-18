608d8b20b3f098507d6def855228e79e7375e04b
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-18 16:19:55 +0900

    Update unless the example folder deleted

--- projects/admin/src/components/index.ts
+++ projects/admin/src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as TitleInput } from './TitleInput';
-export { default as Example } from './Example';
 export { default as ShopItemCard } from './ShopItemCard';
 export { default as ShopCarousel } from './ShopCarousel';
 export { default as Timer } from './Timer';
