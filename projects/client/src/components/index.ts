6e97fd57dabe587ad268c68f946269433319b8fa
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-18 16:15:23 +0900

    Update unless the example folder deleted

--- projects/client/src/components/index.ts
+++ projects/client/src/components/index.ts
@@ -1,4 +1,3 @@
-export { default as Example } from './Example';
 export { default as ShopItem } from './ShopItem';
 export { default as ShopModal } from './ShopModal';
 export { default as ShopModalItem } from './ShopModalItem';
