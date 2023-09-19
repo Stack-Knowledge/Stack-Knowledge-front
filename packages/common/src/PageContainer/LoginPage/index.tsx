8d9f8b6d09d61abe88d29d5aab680defd6a7049c
Lee-Seungje <moondgod@gmail.com>
2023-09-20 00:00:41 +0900

    Delete useless code

--- packages/common/src/PageContainer/LoginPage/index.tsx
+++ packages/common/src/PageContainer/LoginPage/index.tsx
@@ -4,7 +4,7 @@ import * as S from './style';
 import { LoginButton } from 'common/components';
 import { BottomIcon, TopIcon, LoginLogoIcon } from 'common/assets';
 
-import { usePostLoginCode, usePatchAccessToken } from 'api/common';
+import { usePostLoginCode } from 'api/common';
 
 import { useSearchParams } from 'next/navigation';
 
@@ -13,7 +13,6 @@ import { useEffect } from 'react';
 const LoginPage = () => {
   const { get } = useSearchParams();
   const { mutate } = usePostLoginCode();
-  const hook = usePatchAccessToken();
 
   const handleLogin = () => {
     window.location.href = `https://gauth.co.kr/login?client_id=e6e8ac7857c94ca7a24db504d33369078ab562d7a29a4c9db353204ae8080be9&redirect_uri=http://localhost:3000/auth/login/`;
@@ -24,10 +23,6 @@ const LoginPage = () => {
       mutate({ code: get('code') as string });
     }
   }, []);
-
-  const onClick = () => {
-    hook.mutate();
-  };
 
   return (
     <S.MainWrapper>
@@ -40,7 +35,6 @@ const LoginPage = () => {
       <LoginLogoIcon />
       <S.StackKnowledge>Stack Knowledge</S.StackKnowledge>
       <LoginButton onClick={handleLogin} />
-      <h1 onClick={onClick}>이거 누르면 됨</h1>
     </S.MainWrapper>
   );
 };
