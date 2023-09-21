33751a20c1e48cdcda628b8ed8c0407ef46f2b66
Lee-Seungje <moondgod@gmail.com>
2023-09-21 19:46:50 +0900

    Update gradingType

--- packages/types/src/scoringListType.ts
+++ packages/types/src/scoringListType.ts
@@ -1,10 +1,8 @@
+import { Grading } from './grading';
+
 export interface ScoringListType {
   solveId: string;
-  solveStatus: 'NOT_SOLVE' | 'SCORING' | 'CORRECT_ANSWER' | 'WRONG_ANSWER';
-  // NOT_SOLVE : 아직 풀지 않음
-  // SCORING : 채점중
-  // CORRECT_ANSWER : 정답
-  // WRONG_ANSWER : 오답
+  solveStatus: Grading;
   user: {
     id: string;
     email: string;
