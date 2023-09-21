e739b64f3bf1adc9acbddaca38e53e40b5614b9d
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-21 10:00:56 +0900

    Add export admin gradingPage

--- projects/admin/src/PageContainer/index.ts
+++ projects/admin/src/PageContainer/index.ts
@@ -1,3 +1,4 @@
 export { default as ShopPage } from './ShopPage';
 export { default as ScoringPage } from './ScoringPage';
 export { default as CreatePage } from './CreatePage';
+export { default as GradingPage } from './GradingPage';
