c257d199d044e950cac7915d9c5d6ac3a1579aec
Lee-Seungje <moondgod@gmail.com>
2023-09-21 13:45:41 +0900

    Add data

--- packages/common/src/components/RankingList/index.tsx
+++ packages/common/src/components/RankingList/index.tsx
@@ -5,24 +5,30 @@ import { RankingItem } from 'common/components';
 
 import { RankingPropsType } from 'types';
 
+import { useGetRankingList } from 'api/common';
+
 interface RankingListProps {
   list: RankingPropsType[];
 }
 
-const RankingList: React.FC<RankingListProps> = ({ list }) => (
-  <S.RankingListWrapper>
-    <div>
-      <S.RankingText>랭킹</S.RankingText>
-      <S.ItemListWrapper>
-        {list.map((item, index) => (
-          <>
-            <RankingItem key={item.id} item={item} ranking={index + 1} />
-            {index !== list.length - 1 && <S.Line />}
-          </>
-        ))}
-      </S.ItemListWrapper>
-    </div>
-  </S.RankingListWrapper>
-);
+const RankingList: React.FC<RankingListProps> = ({ list }) => {
+  const { data } = useGetRankingList();
+
+  return (
+    <S.RankingListWrapper>
+      <div>
+        <S.RankingText>랭킹</S.RankingText>
+        <S.ItemListWrapper>
+          {data?.map((item, index) => (
+            <>
+              <RankingItem key={item.id} item={item} ranking={index + 1} />
+              {index !== list.length - 1 && <S.Line />}
+            </>
+          ))}
+        </S.ItemListWrapper>
+      </div>
+    </S.RankingListWrapper>
+  );
+};
 
 export default RankingList;
