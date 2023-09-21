6363d7016b10d0ad747eaf37f59b8744944bc327
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-22 01:25:28 +0900

    Update usePostSolve feat

---
+++ packages/api/client/src/hooks/solve/usePostSolve.ts
@@ -0,0 +1,24 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { post, solveQueryKeys, getHeaders, solveUrl } from 'api/common';
+
+export const usePostSolve = (
+  onSuccessFunc: () => void,
+  missionId: string,
+  solveId: string
+) =>
+  useMutation<
+    void,
+    Error,
+    {
+      solvation: string;
+    }
+  >(
+    solveQueryKeys.postSolve(missionId),
+    (newSolve) => post(solveUrl.solve(solveId), newSolve, getHeaders()),
+    {
+      onSuccess: () => {
+        onSuccessFunc();
+      },
+    }
+  );
