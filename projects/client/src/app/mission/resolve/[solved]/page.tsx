9e32461e3aa35f19d91549f9a5ce23a6c6f59199
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-25 23:30:09 +0900

    Update error conflict

--- projects/client/src/app/mission/resolve/[solved]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MissionDetailPage } from 'client/PageContainer';
-
-interface SolveProps {
-  params: { missionId: string };
-}
-
-export default function SolvePage({ params: { missionId } }: SolveProps) {
-  return <MissionDetailPage missionId={missionId} />;
-}
