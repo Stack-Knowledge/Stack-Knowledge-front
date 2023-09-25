cdd8e710b872421850afa55628f51da9c9d9fd7b
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-09-25 23:30:40 +0900

    Fix components name change

--- projects/client/src/PageContainer/MissionDetailPage/index.tsx
+++ projects/client/src/PageContainer/MissionDetailPage/index.tsx
@@ -1,15 +1,11 @@
 'use client';
 
 import { MissionDetailInput } from 'common';
-
 import { MissionDetailModal } from 'client/components';
 import { Timer } from 'client/components';
-
 import * as S from './style';
-
 import { useGetMissionDetail } from 'api/common';
-
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { usePostSolve } from 'api/client';
 import { useRouter } from 'next/navigation';
 
@@ -19,27 +15,26 @@ interface MissionDetailProps {
 
 const MissionDetailPage: React.FC<MissionDetailProps> = ({ missionId }) => {
   const { push } = useRouter();
-
   const dialog = useRef<HTMLDialogElement>(null);
-
   const [inputValue, setInputValue] = useState<string>('');
   const [hours, setHours] = useState<number>(0);
   const [minutes, setMinutes] = useState<number>(0);
-
-  const { data } = useGetMissionDetail(missionId);
 
   const onSuccessFunc = () => {
     push('/');
     alert('문제가 등록되었습니다 !');
   };
 
+  const { data } = useGetMissionDetail(missionId);
+
   const { mutate } = usePostSolve(onSuccessFunc, missionId);
 
-  if (!data) push('/');
-  else {
-    setHours(Math.floor(data.timeLimit / 3600));
-    setMinutes(Math.floor((data.timeLimit % 3600) / 60));
-  }
+  useEffect(() => {
+    if (data) {
+      setHours(Math.floor(data.timeLimit / 3600));
+      setMinutes(Math.floor((data.timeLimit % 3600) / 60));
+    }
+  }, [data]);
 
   const handleSubmit = () => {
     if (inputValue)
