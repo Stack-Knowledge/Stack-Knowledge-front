acc9df4257f76dc5e3dd0df0ba706aa8e70a201c
Lee-Seungje <moondgod@gmail.com>
2023-09-27 09:25:23 +0900

    Add getData

--- projects/admin/src/components/MissionCarousel/index.tsx
+++ projects/admin/src/components/MissionCarousel/index.tsx
@@ -5,26 +5,41 @@ import * as S from './style';
 import { VectorIcon } from 'common/assets';
 import { TaskCard } from 'common/components';
 
+import type { ScoringListType } from 'types';
+
 import { useGetScoringList } from 'api/admin';
 
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const MissionCarousel = () => {
   const [pageIndex, setPageIndex] = useState(0);
+  const [data, setData] = useState<ScoringListType[] | undefined>([]);
 
-  const { data } = useGetScoringList(pageIndex);
+  const { data: newData } = useGetScoringList(pageIndex);
   const { push } = useRouter();
 
   const onCardClick = (solveId: string) => {
     push(`mission/scoring/${solveId}`);
   };
 
+  const moveRight = () => {
+    if (data?.length === 10) setPageIndex((cur) => cur + 1);
+  };
+
+  const moveLeft = () => {
+    if (pageIndex > 0) setPageIndex((cur) => cur - 1);
+  };
+
+  useEffect(() => {
+    setData(newData);
+  }, [newData]);
+
   return (
     <>
       {data?.length != 0 && (
         <S.CarouselWrapper>
-          <S.PointerWrapper>
+          <S.PointerWrapper onClick={moveLeft}>
             <VectorIcon direction='left' />
           </S.PointerWrapper>
           <S.ContentWrapper>
@@ -39,7 +54,7 @@ const MissionCarousel = () => {
               />
             ))}
           </S.ContentWrapper>
-          <S.PointerWrapper>
+          <S.PointerWrapper onClick={moveRight}>
             <VectorIcon direction='right' />
           </S.PointerWrapper>
         </S.CarouselWrapper>
