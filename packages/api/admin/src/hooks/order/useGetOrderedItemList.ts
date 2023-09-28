224fabeb695cce52a0bcf835ad7d80d49928d240
Lee-Seungje <moondgod@gmail.com>
2023-09-28 17:11:34 +0900

    Add useGetOrderedItemList

---
+++ packages/api/admin/src/hooks/order/useGetOrderedItemList.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { orderQueryKeys, orderUrl, get } from 'api/common';
+
+import type { OrderdItemType } from 'types';
+
+import type { UseQueryOptions } from '@tanstack/react-query';
+
+export const useGetRankingList = (
+  options?: UseQueryOptions<OrderdItemType[]>
+) =>
+  useQuery<OrderdItemType[]>(
+    orderQueryKeys.getOrderedItemList(),
+    () => get(orderUrl.orderedItemList()),
+    options
+  );
