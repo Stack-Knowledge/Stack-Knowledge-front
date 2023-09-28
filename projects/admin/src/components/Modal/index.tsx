523709861711a48afe730c30e62f893b793f9759
Lee-Seungje <moondgod@gmail.com>
2023-09-28 23:48:18 +0900

    Delete return

--- projects/admin/src/components/Modal/index.tsx
+++ projects/admin/src/components/Modal/index.tsx
@@ -6,18 +6,16 @@ interface ModalProps {
   onClick: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ content, onClick }) => {
-  return (
-    <S.ModalWrapper>
-      <S.ModalContentWrapper>
-        <S.ModalContent>{content}</S.ModalContent>
-      </S.ModalContentWrapper>
-      <S.ModalButtonWrapper method='dialog'>
-        <S.ModalDeleted>취소</S.ModalDeleted>
-        <S.ModalConfirm onClick={onClick}>확인</S.ModalConfirm>
-      </S.ModalButtonWrapper>
-    </S.ModalWrapper>
-  );
-};
+const Modal: React.FC<ModalProps> = ({ content, onClick }) => (
+  <S.ModalWrapper>
+    <S.ModalContentWrapper>
+      <S.ModalContent>{content}</S.ModalContent>
+    </S.ModalContentWrapper>
+    <S.ModalButtonWrapper method='dialog'>
+      <S.ModalDeleted>취소</S.ModalDeleted>
+      <S.ModalConfirm onClick={onClick}>확인</S.ModalConfirm>
+    </S.ModalButtonWrapper>
+  </S.ModalWrapper>
+);
 
 export default Modal;
