458fb5860d203db05eb9bca83d110da3c2b8e358
Lee-Seungje <moondgod@gmail.com>
2023-09-29 09:29:57 +0900

    Update prevent navigation

--- packages/common/src/components/Header/index.tsx
+++ packages/common/src/components/Header/index.tsx
@@ -12,6 +12,8 @@ import {
   MadeIcon,
 } from 'common/assets';
 
+import { useEffect, useState } from 'react';
+
 interface HeaderProps {
   role: 'admin' | 'client';
 }
@@ -20,40 +22,52 @@ const Header: React.FC<HeaderProps> = ({ role }) => {
   const pathname = usePathname();
   if (pathname === '/auth/login') return <></>;
 
+  const handleLinkClick = (e: any) => {
+    if (pathname.includes('/mission/resolve')) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <S.HeaderWrapper>
       <S.HeaderContainer>
-        <S.LogoContainer>
+        <S.LogoContainer href='/' onClick={handleLinkClick}>
           <LogoIcon />
           <S.Title>Stack Knowledge</S.Title>
         </S.LogoContainer>
         <S.MenuNav role={role}>
-          <S.MenuItemWrapper href='/'>
+          <S.MenuItemWrapper href='/' onClick={handleLinkClick}>
             <HomeIcon />
             <S.ItemTitle>홈</S.ItemTitle>
           </S.MenuItemWrapper>
           {role === 'admin' ? (
             <>
-              <S.MenuItemWrapper href='/mission/scoring'>
+              <S.MenuItemWrapper
+                href='/mission/scoring'
+                onClick={handleLinkClick}
+              >
                 <QuestionIcon />
                 <S.ItemTitle>채점하기</S.ItemTitle>
               </S.MenuItemWrapper>
-              <S.MenuItemWrapper href='/mission/create'>
+              <S.MenuItemWrapper
+                href='/mission/create'
+                onClick={handleLinkClick}
+              >
                 <MadeIcon />
                 <S.ItemTitle>만들기</S.ItemTitle>
               </S.MenuItemWrapper>
             </>
           ) : (
-            <S.MenuItemWrapper href='/mission/list'>
+            <S.MenuItemWrapper href='/mission/list' onClick={handleLinkClick}>
               <QuestionIcon />
               <S.ItemTitle>문제</S.ItemTitle>
             </S.MenuItemWrapper>
           )}
-          <S.MenuItemWrapper href='/shop'>
+          <S.MenuItemWrapper href='/shop' onClick={handleLinkClick}>
             <ShopIcon />
             <S.ItemTitle>상점</S.ItemTitle>
           </S.MenuItemWrapper>
-          <S.MenuItemWrapper href='/ranking'>
+          <S.MenuItemWrapper href='/ranking' onClick={handleLinkClick}>
             <RankingIcon />
             <S.ItemTitle>랭킹</S.ItemTitle>
           </S.MenuItemWrapper>
