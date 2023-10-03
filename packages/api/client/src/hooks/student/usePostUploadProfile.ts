5ca09ffc224af05a9d3a9ad9953275363cce31a4
Lee-Seungje <moondgod@gmail.com>
2023-10-03 17:04:18 +0900

    Update usePostUploadProfile

--- packages/api/client/src/hooks/student/usePostUploadProfile.ts
+++ packages/api/client/src/hooks/student/usePostUploadProfile.ts
@@ -3,10 +3,12 @@ import { useMutation } from '@tanstack/react-query';
 import { post, studentQueryKeys, studentUrl } from 'api/common';
 
 export const usePostUploadProfile = () =>
-  useMutation(studentQueryKeys.postUploadProfile(), (uploadProfile: FormData) =>
-    post(studentUrl.uploadProfile(), uploadProfile, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
+  useMutation<{ profileImage: string }, Error, FormData>(
+    studentQueryKeys.postUploadProfile(),
+    (uploadProfile) =>
+      post(studentUrl.uploadProfile(), uploadProfile, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
   );
