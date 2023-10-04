63cfd7fd4fa47371570e5fb372c31e152b56e889
Lee-Seungje <moondgod@gmail.com>
2023-10-04 11:21:21 +0900

    Add generateMetadata

--- projects/client/src/app/mission/resolve/[missionId]/page.tsx
+++ projects/client/src/app/mission/resolve/[missionId]/page.tsx
@@ -1,8 +1,28 @@
 import { MissionDetailPage } from 'client/PageContainer';
 
+import { useGetMissionDetail } from 'api/common';
+
+import { Metadata } from 'next';
+
 interface SolveProps {
   params: { missionId: string };
 }
+
+export const generateMetadata = async ({
+  params: { missionId },
+}: SolveProps): Promise<Metadata> => {
+  const { data } = await useGetMissionDetail(missionId);
+
+  return {
+    title: { absolute: data?.title ?? '' },
+    description: data?.content,
+    openGraph: {
+      title: data?.title,
+      description: data?.content,
+      url: `https://stack-knowledge-client.vercel.app/mission/resolve/${missionId}`,
+    },
+  };
+};
 
 export default function SolvePage({ params: { missionId } }: SolveProps) {
   return <MissionDetailPage missionId={missionId} />;
