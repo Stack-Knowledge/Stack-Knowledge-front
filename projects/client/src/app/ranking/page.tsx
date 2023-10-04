665ffc031e5088eea1018511f424c910a80b0dec
Lee-Seungje <moondgod@gmail.com>
2023-10-04 11:22:17 +0900

    Add rankingPage SEO

--- projects/client/src/app/ranking/page.tsx
+++ projects/client/src/app/ranking/page.tsx
@@ -1,5 +1,17 @@
 import { RankingPage } from 'client/PageContainer';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  description: 'StackKnowledge 랭킹 페이지입니다.',
+  title: { absolute: '랭킹' },
+  openGraph: {
+    title: '랭킹',
+    description: 'StackKnowledge 랭킹 페이지입니다.',
+    url: 'https://stack-knowledge-client.vercel.app/ranking/',
+  },
+};
+
 export default function Ranking() {
   return <RankingPage />;
 }
