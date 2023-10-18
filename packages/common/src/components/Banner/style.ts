f6657d0cb38a9f1ead3d2dbfb9da265b8780c892
Lee-Seungje <moondgod@gmail.com>
2023-10-18 20:30:34 +0900

    Update mainPage layout

--- packages/common/src/components/Banner/style.ts
+++ packages/common/src/components/Banner/style.ts
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled';
 
 export const BannerWrapper = styled.div`
-  width: 80rem;
+  max-width: 80rem;
+  width: 100%;
   height: 18.75rem;
   border-radius: 1.25rem;
   display: flex;
