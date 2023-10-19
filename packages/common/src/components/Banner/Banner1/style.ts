dc14f31c78ec3409c767e3b41ddca788129331eb
Lee-Seungje <moondgod@gmail.com>
2023-10-19 16:08:43 +0900

    Update banner1

--- packages/common/src/components/Banner/Banner1/style.ts
+++ packages/common/src/components/Banner/Banner1/style.ts
@@ -16,12 +16,19 @@ export const BannerTitleContainer = styled.div`
   width: 40rem;
   height: 18.75rem;
   padding: 4.375rem 0 0 3.75rem;
+  @media (max-width: 1240px) {
+    width: 50vw;
+  }
 `;
 
 export const BannerTitle = styled.span`
   color: ${({ theme }) => theme.color.white};
   ${({ theme }) => theme.typo.h1};
   font-weight: 700;
+
+  @media (max-width: 1240px) {
+    ${({ theme }) => theme.typo.h2};
+  }
 `;
 
 export const SunIconWrapper = styled.div`
@@ -30,16 +37,30 @@ export const SunIconWrapper = styled.div`
   right: 1.25rem;
   width: 18.1875rem;
   height: 12rem;
+
+  @media (max-width: 1240px) {
+    width: 20vw;
+    height: 13vw;
+  }
 `;
 
 export const LetgoIconContainer = styled.div`
   width: 40rem;
   height: 18.75rem;
-  padding: 1.5rem 3.75rem 1rem 11.25rem;
+  padding: 2.5rem 3.75rem 1rem 11.25rem;
+
+  @media (max-width: 1240px) {
+    padding: 3.8rem 0 1rem 20vw;
+  }
 `;
 
 export const LetgoIconWrapper = styled.div`
   position: relative;
   width: 25rem;
   height: 16.25rem;
+
+  @media (max-width: 1240px) {
+    width: 22vw;
+    height: 16vw;
+  }
 `;
