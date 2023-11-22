edccb3c8ab4685c79286f1d2cf871426fe7eb27c
TaerangLee <s22051@gsm.hs.kr>
2023-11-22 11:03:57 +0900

    Update header svg unit change px -> rem

--- packages/common/src/assets/svg/QuestionIcon.tsx
+++ packages/common/src/assets/svg/QuestionIcon.tsx
@@ -1,7 +1,7 @@
 const QuestionIcon = () => (
   <svg
-    width='20'
-    height='22'
+    width='1.25rem'
+    height='1.375rem'
     viewBox='0 0 20 22'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
