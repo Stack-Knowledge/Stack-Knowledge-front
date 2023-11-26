35160bd0e7627d99b4ffba613804450576680388
TaerangLee <s22051@gsm.hs.kr>
2023-11-27 00:38:22 +0900

    Update contentWrapper responsive

--- projects/client/src/components/MissionCarousel/style.ts
+++ projects/client/src/components/MissionCarousel/style.ts
@@ -11,6 +11,13 @@ export const ContentWrapper = styled.div`
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: repeat(2, 1fr);
   gap: 4.125rem 3rem;
+
+  @media ${({ theme }) => theme.breakPoint[1024]} {
+    width: calc(100vw - 20rem);
+  }
+  @media (max-width: 750px) {
+    width: calc(100vw - 18.5rem);
+  }
 `;
 
 export const PointerWrapper = styled.div`
