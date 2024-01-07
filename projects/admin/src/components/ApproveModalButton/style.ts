e79a4aa4e118c4c9ff776c604f96a48d49a9814c
Lee-Seungje <moondgod@gmail.com>
2024-01-08 00:04:20 +0900

    Add animation

--- projects/admin/src/components/ApproveModalButton/style.ts
+++ projects/admin/src/components/ApproveModalButton/style.ts
@@ -4,4 +4,19 @@ export const ButtonWrapper = styled.div`
   position: absolute;
   right: 10vw;
   top: 45vh;
+  cursor: pointer;
+  transition: ease-in-out transform 0.2s;
+
+  :hover {
+    transform: rotate(180deg);
+  }
+
+  svg {
+    transition: ease-in-out box-shadow 0.2s;
+    border-radius: 50%;
+
+    :hover {
+      box-shadow: 0 0 0.625rem 0.3125rem ${({ theme }) => theme.color.primary};
+    }
+  }
 `;
