b00b3a850078574ff5c50efc90a6ad7d9a3f94f1
TaerangLee <s22051@gsm.hs.kr>
2024-01-08 22:33:26 +0900

    Update approvedModal page

--- projects/admin/src/PageContainer/MainPage/index.tsx
+++ projects/admin/src/PageContainer/MainPage/index.tsx
@@ -2,14 +2,30 @@
 
 import { useRef, useState } from 'react';
 
-import { XIcon } from 'admin/assets';
+import { XIcon, BarIcon } from 'admin/assets';
+
+import { useGetApprovedList, usePatchApproved } from 'api/admin';
+
 import { ApproveModalButton } from 'admin/components';
 
 import { MainPage } from 'common';
 
 import * as S from './style';
 
-const MainPageComponent = () => {
+interface MainPageProps {
+  userId: string;
+}
+
+const MainPageComponent: React.FC<MainPageProps> = ({ userId }) => {
+  const { mutate, isSuccess } = usePatchApproved(userId);
+  const { data } = useGetApprovedList();
+
+  if (isSuccess) {
+    alert('성공');
+  } else {
+    console.log('실패');
+  }
+
   const dialog = useRef<HTMLDialogElement>(null);
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -19,6 +35,22 @@ const MainPageComponent = () => {
     setIsOpen(true);
   };
 
+  const formatDate = (isoDate: string) => {
+    const options = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    } as Intl.DateTimeFormatOptions;
+    const formattedDate = new Date(isoDate).toLocaleDateString(
+      'en-US',
+      options
+    );
+
+    const [month, day, year] = formattedDate.split('/');
+
+    return `${year}.${month}.${day}`;
+  };
+
   return (
     <>
       <S.Modal ref={dialog} isOpen={isOpen}>
@@ -26,6 +58,30 @@ const MainPageComponent = () => {
           <S.ModalButton onClick={() => setIsOpen(false)}>
             <XIcon />
           </S.ModalButton>
+          <S.ModalWrapper>
+            {data &&
+              data.map((item, index) => (
+                <S.ModalItem key={index}>
+                  <S.TitleContainer>
+                    <S.Title>{item.name}</S.Title>
+                    <BarIcon />
+                    <S.Title>{formatDate(item.createdAt)}</S.Title>
+                  </S.TitleContainer>
+                  <S.ApprovedContainer>
+                    <S.ApprovedButton
+                      onClick={() => mutate({ approveStatus: 'APPROVED' })}
+                    >
+                      수락
+                    </S.ApprovedButton>
+                    <S.ApprovedButton
+                      onClick={() => mutate({ approveStatus: 'REJECT' })}
+                    >
+                      거절
+                    </S.ApprovedButton>
+                  </S.ApprovedContainer>
+                </S.ModalItem>
+              ))}
+          </S.ModalWrapper>
         </form>
       </S.Modal>
       <S.Wrapper onClick={handleModalOpen}>
--- projects/admin/src/PageContainer/MainPage/style.ts
+++ projects/admin/src/PageContainer/MainPage/style.ts
@@ -11,7 +11,7 @@ const slideAndScaleAnimation = keyframes`
 `;
 
 export const Modal = styled.dialog<{ isOpen: boolean }>`
-  width: 21.875rem;
+  width: fit-content;
   height: 24.0625rem;
   border-radius: 1.25rem;
   background: #fbfbfb;
@@ -42,7 +42,58 @@ export const ModalButton = styled.button`
 
 export const Wrapper = styled.div`
   position: absolute;
-  right: 20vw;
-  top: 75vh;
+  right: 9vw;
+  top: 88vh;
   z-index: 3;
+`;
+
+export const ModalWrapper = styled.div`
+  width: 100%;
+  height: 24.0625rem;
+  padding: 2rem 3.1875rem 2rem 2.5625rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.625rem;
+`;
+
+export const ModalItem = styled.div`
+  width: 100%;
+  align-items: center;
+  display: flex;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.375rem;
+  width: 100%;
+`;
+
+export const Title = styled.span`
+  ${({ theme }) => theme.typo.body2};
+  color: ${({ theme }) => theme.color.black};
+`;
+
+export const ApprovedContainer = styled.div`
+  display: flex;
+  margin-left: 20px;
+  gap: 0.5rem;
+`;
+
+export const ApprovedButton = styled.div`
+  background-color: ${({ theme }) => theme.color.gray['020']};
+  color: ${({ theme }) => theme.color.black};
+  ${({ theme }) => theme.typo.body3};
+  display: flex;
+  border-radius: 0.125rem;
+  justify-content: center;
+  align-items: center;
+  width: 2.8125rem;
+  height: 1.3125rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.primary};
+    color: ${({ theme }) => theme.color.white};
+  }
 `;
