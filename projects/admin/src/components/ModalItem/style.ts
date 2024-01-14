927d3540b4ad117dbf9fa091343f9467be3327c8
TaerangLee <s22051@gsm.hs.kr>
2024-01-14 19:55:55 +0900

    Update modalItem style workspace sort

--- projects/admin/src/components/ModalItem/style.ts
+++ projects/admin/src/components/ModalItem/style.ts
@@ -1,29 +1,5 @@
 import styled from '@emotion/styled';
 
-export const ModalItem = styled.div`
-  width: 100%;
-  align-items: center;
-  display: flex;
-`;
-
-export const TitleContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 0.375rem;
-  width: 100%;
-`;
-
-export const Title = styled.span`
-  ${({ theme }) => theme.typo.body2};
-  color: ${({ theme }) => theme.color.black};
-`;
-
-export const ApprovedContainer = styled.div`
-  display: flex;
-  margin-left: 20px;
-  gap: 0.5rem;
-`;
-
 export const ApprovedButton = styled.div`
   background-color: ${({ theme }) => theme.color.gray['020']};
   color: ${({ theme }) => theme.color.black};
@@ -40,4 +16,28 @@ export const ApprovedButton = styled.div`
     background-color: ${({ theme }) => theme.color.primary};
     color: ${({ theme }) => theme.color.white};
   }
+`;
+
+export const ApprovedContainer = styled.div`
+  display: flex;
+  margin-left: 20px;
+  gap: 0.5rem;
+`;
+
+export const ModalItem = styled.div`
+  width: 100%;
+  align-items: center;
+  display: flex;
+`;
+
+export const Title = styled.span`
+  ${({ theme }) => theme.typo.body2};
+  color: ${({ theme }) => theme.color.black};
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.375rem;
+  width: 100%;
 `;
