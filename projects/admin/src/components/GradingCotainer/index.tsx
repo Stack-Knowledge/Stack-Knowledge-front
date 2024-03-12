dcd7c7908f3eb3f7f6ff26881296b9c30cd5db2f
Lee-Seungje <moondgod@gmail.com>
2024-03-12 15:09:38 +0900

    Update style

--- projects/admin/src/components/GradingCotainer/index.tsx
+++ projects/admin/src/components/GradingCotainer/index.tsx
@@ -5,15 +5,21 @@ import * as S from './style';
 interface GradingProps {
   children: React.ReactNode;
   onClick: () => void;
+  onAiClick: () => void;
 }
 
-const GradingContainer: React.FC<GradingProps> = ({ children, onClick }) => (
+const GradingContainer: React.FC<GradingProps> = ({
+  children,
+  onClick,
+  onAiClick,
+}) => (
   <S.GradingtContainer>
     <S.MissionDetailInputWrapper>
       <S.GradingWrapper>{children}</S.GradingWrapper>
     </S.MissionDetailInputWrapper>
     <S.SubmitButtonWrapper>
       <S.SubmitButton onClick={onClick}>제출하기</S.SubmitButton>
+      <S.AIButton onClick={onAiClick}>AI로 채첨하기</S.AIButton>
     </S.SubmitButtonWrapper>
   </S.GradingtContainer>
 );
--- projects/admin/src/components/GradingCotainer/style.ts
+++ projects/admin/src/components/GradingCotainer/style.ts
@@ -1,17 +1,5 @@
 import styled from '@emotion/styled';
 
-export const GradingtContainer = styled.div`
-  background: ${({ theme }) => theme.color.gray['010']};
-  width: 59.375rem;
-  height: 18.75rem;
-  border-radius: 0.625rem;
-  box-shadow: 0.1875rem 0.1875rem 0.25rem 0rem rgba(120, 120, 120, 0.25);
-`;
-export const MissionDetailInputWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
 export const GradingWrapper = styled.div`
   background: ${({ theme }) => theme.color.gray['010']};
   ${({ theme }) => theme.typo.body1};
@@ -20,11 +8,17 @@ export const GradingWrapper = styled.div`
   height: 13.75rem;
   margin-top: 1.375rem;
 `;
+export const GradingtContainer = styled.div`
+  background: ${({ theme }) => theme.color.gray['010']};
+  width: 59.375rem;
+  height: 18.75rem;
+  border-radius: 0.625rem;
+  box-shadow: 0.1875rem 0.1875rem 0.25rem 0rem rgba(120, 120, 120, 0.25);
+`;
 
-export const SubmitButtonWrapper = styled.div`
+export const MissionDetailInputWrapper = styled.div`
   display: flex;
-  justify-content: flex-end;
-  margin: 0 2.375rem 1.25rem 0;
+  justify-content: center;
 `;
 
 export const SubmitButton = styled.button`
@@ -37,4 +31,15 @@ export const SubmitButton = styled.button`
   font-weight: 400;
   border: 0;
   cursor: pointer;
+`;
+
+// eslint-disable-next-line sort-exports/sort-exports
+export const AIButton = styled(SubmitButton)`
+  margin-left: 12px;
+`;
+
+export const SubmitButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin: 0 2.375rem 1.25rem 0;
 `;
